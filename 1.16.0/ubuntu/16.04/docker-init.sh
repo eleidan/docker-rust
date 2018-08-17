2ced3d63cf973778b15e91f9af777461d8302005
Oleg Kulyk <helh.saintman@gmail.com>
2018-08-17 14:33:48 +0300

    Drop 1.16.0

--- 1.16.0/ubuntu/16.04/docker-init.sh
+++
@@ -1,6 +0,0 @@
-#!/bin/sh
-
-DOCKER_FILES_DIR="$HOME/recipes"
-echo "Copying files from $DOCKER_FILES_DIR (container) to project directory (host) ..." \
-  && cp -r $DOCKER_FILES_DIR/* . \
-  && echo "Done."
